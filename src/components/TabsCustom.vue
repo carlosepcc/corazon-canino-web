97bfebb169b517465f0825875d577d3ac058523f
Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2022-10-29 21:16:37 -0400

    fix:tab label

--- src/components/TabsCustom.vue
+++ src/components/TabsCustom.vue
@@ -1,12 +1,13 @@
 <template>
   <q-tabs align="justify" :inline-label="!mobileMode" :dense="mobileMode" narrow-indicator>
     <!-- <q-route-tab to="/" label="Inicio" /> -->
-    <q-route-tab to="/" :label="mobileMode ? '' : 'Inicio'" icon="pets" />
-    <q-route-tab to="/about" :label="mobileMode ? '' : 'Acerca'" icon="info" />
-    <q-route-tab to="/contact" :label="mobileMode ? '' : 'Contacto'" icon="chat" />
+    <q-route-tab to="/" label="Inicio" icon="pets" />
+    <q-route-tab to="/about" label="Acerca" icon="info" />
+    <q-route-tab to="/contact" label="Contacto" icon="chat" />
   </q-tabs>
 </template>
 <script setup lang="ts">
 import s from 'src/composables/useState'
 const props = defineProps({ mobileMode: { type: Boolean, default: s.value.isOnMobile } })
+
 </script>
