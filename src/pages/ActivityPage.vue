3fb143730ca32da9a6c5d93568350909543cc6a2
Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2022-10-30 23:36:33 -0400

    fix:carousel

--- src/pages/ActivityPage.vue
+++ src/pages/ActivityPage.vue
@@ -4,28 +4,43 @@
     <span class="text-h7">NUESTRA ACTIVIDAD</span>
     <div class="row items-center justify-evenly" padding>
       <iframe
-        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCanicoUCI%2Fposts%2Fpfbid0LZx9gx4vs6gBiBhNJLpsMffWcapvwuZBD1KeLz8z4sjq92dMpvzstcH9JydGnTQel&show_text=true&width=500"
-        width="500" height="500" frameborder="0" allowfullscreen="true"
-        allow="clipboard-write; encrypted-media; picture-in-picture; web-share">No se pudo
-        mostrar
-        el post de Facebook..</iframe>
+        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCaninoUCI%2Fposts%2Fpfbid0LZx9gx4vs6gBiBhNJLpsMffWcapvwuZBD1KeLz8z4sjq92dMpvzstcH9JydGnTQel&show_text=true&width=500"
+        width="500"
+        height="500"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="clipboard-write; encrypted-media; picture-in-picture; web-share"
+        >No se pudo mostrar el post de Facebook..</iframe
+      >
       <iframe
-        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCanicoUCI%2Fposts%2Fpfbid02VUWmT7RenPRJSPWGfAFJ1aNLBkiLLKATtCwKwfPf1QXvbTeartoiQaURLBPQvK8Sl&show_text=true&width=500"
-        width="500" height="500" frameborder="0" allowfullscreen="true"
-        allow="clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCaninoUCI%2Fposts%2Fpfbid02VUWmT7RenPRJSPWGfAFJ1aNLBkiLLKATtCwKwfPf1QXvbTeartoiQaURLBPQvK8Sl&show_text=true&width=500"
+        width="500"
+        height="500"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="clipboard-write; encrypted-media; picture-in-picture; web-share"
+      ></iframe>
       <iframe
-        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCanicoUCI%2Fposts%2Fpfbid02G17jnjDVpKr6QXhHvpWyEwHiK6WY49v7boHZunnhndfpaxS4TFQGFmCt9qqreYYkl&show_text=true&width=500"
-        width="500" height="805" frameborder="0" allowfullscreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCaninoUCI%2Fposts%2Fpfbid02G17jnjDVpKr6QXhHvpWyEwHiK6WY49v7boHZunnhndfpaxS4TFQGFmCt9qqreYYkl&show_text=true&width=500"
+        width="500"
+        height="805"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      ></iframe>
       <iframe
-        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCanicoUCI%2Fposts%2Fpfbid02xWSgNp9yP85uksFb13E9NLjNjqxBL62pEyKq6zti6xqQZSZV4h5PqCHrUnap4Qusl&show_text=true&width=500"
-        width="500" height="500" frameborder="0" allowfullscreen="true"
-        allow="clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCaninoUCI%2Fposts%2Fpfbid02xWSgNp9yP85uksFb13E9NLjNjqxBL62pEyKq6zti6xqQZSZV4h5PqCHrUnap4Qusl&show_text=true&width=500"
+        width="500"
+        height="500"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="clipboard-write; encrypted-media; picture-in-picture; web-share"
+      ></iframe>
     </div>
   </q-page>
 </template>
 <script setup lang="ts">
-import CarouselCustom from 'components/CarouselCustom.vue'
+import CarouselCustom from 'components/CarouselCustom.vue';
 </script>
 <style scoped lang="sass">
 iframe
