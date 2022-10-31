a3812327355d019828d90018dfd279cfa4acce5e
Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2022-10-31 11:00:43 -0400

    add: post erza

--- src/pages/ActivityPage.vue
+++ src/pages/ActivityPage.vue
@@ -60,6 +60,16 @@
         allowfullscreen="true"
         allow="clipboard-write; encrypted-media; picture-in-picture; web-share"
       ></iframe>
+      <iframe
+        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FCorazonCaninoUCI%2Fposts%2Fpfbid02aTbTUWmVXsULkppLJgWaQUkFkb6H3JZ5spMC69pHhBJNqyrb2w5qvpFrebuSrF5Vl&show_text=true&width=500"
+        width="500"
+        height="250"
+        style="border: none; overflow: hidden"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      ></iframe>
     </div>
   </q-page>
 </template>
