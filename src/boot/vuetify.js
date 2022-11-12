c6c1290017f63bd2f67fc5dbfaac34d6a853d7b1
Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2022-11-12 02:52:28 -0500

    vuetify boot (not working)

---
+++ src/boot/vuetify.js
@@ -0,0 +1,18 @@
+import { boot } from 'quasar/wrappers';
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async (/* { app, router, ... } */ { app }) => {
+  // something to do
+  app.use(vuetify);
+});
